62eb690244fffbbac6980e557e190838f5ccf58c
iceicewalker <dabarreno@hotmail.com>
2022-12-15 09:28:44 -0500

    Null Config

--- artemisa-front/src/environments/environment.prod.ts
+++ artemisa-front/src/environments/environment.prod.ts
@@ -1,14 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyD5jqetQcOmf3BcK7VElV_UUOatNIVeM38",
-    authDomain: "artemisa-ec.firebaseapp.com",
-    projectId: "artemisa-ec",
-    storageBucket: "artemisa-ec.appspot.com",
-    messagingSenderId: "596800043027",
-    appId: "1:596800043027:web:98ff9b383e0eead1c4e7e6",
-    measurementId: "G-4G2HMKS8GC"
-  },
-  whatsAppKey: "EAAHP3V3xhSEBAHL2cOhkuOiuT1FNbDOrjnDXIby7hdHmvVzdHCJUP3zjAkum8N6ZApr0ED5oc03LZC86NqG9u5EYY18RZB74fk6YBxMId0MyLStToCPs1Pz0kSbnHqRxXPxgpEY3QYhVQlxeuxplF18pOgxO1EuhHglaEbmrUI0iAZCfUgIrTUWQLOU1VXh4LZA35lx6gOwZDZD", //"EAAHP3V3xhSEBAGq2gGNCTuWbPEEpiDBFjgah9tO5EZBYfqTSfXDPZCrM60h755ZC7r1ZAN8ITzgtnn4oH4ij28Veh0u938lZAgy9HRJBWrqtSwZB9yTP3IPoa8FiEmAm6kZBZCX9yNqcIZCWvtwACTSmR6nMOZBKOxJsYxSxcdsZA1e3Q5iNZB6KQwc3",
-  whatsAppId: 101136095987590
+
 };
